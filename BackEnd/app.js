bce7eb3bac210fbdf07e9f3e5a5e13cfe43f2f86
Cristian Alegria <abraham.32v@gmail.com>
2023-07-12 15:25:10 -0600

    cambios src_bak return

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,16 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRoute = require("./routes/users");
 const cardsRoute = require("./routes/cards");
-const {PORT=3000} = process.env
+const { PORT = 3000 } = process.env;
 const cors = require("cors");
 const { celebrate, Joi, errors } = require("celebrate");
-const { login, createUser} = require("./controllers/users");
+const { login, createUser } = require("./controllers/users");
 const auth = require("./middlewares/auth");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1/aroundb");
+mongoose
+  .connect("mongodb://127.0.0.1/aroundb")
+  .then(console.log("me he conectado al servidor :)"))
 
 app.use(express.json());
 app.use(cors());
@@ -19,25 +21,33 @@ app.options("*", cors());
 
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
+app.get("/crash-test", () => {
   setTimeout(() => {
-    throw new Error('El servidor va a caer');
+    throw new Error("El servidor va a caer");
   }, 0);
-})
+});
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+app.post(
+  "/signin",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
   }),
-}), login);
+  login
+);
 
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+app.post(
+  "/signup",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
   }),
-}), createUser);
+  createUser
+);
 
 app.use(auth);
 app.use("/users", userRoute);
@@ -47,24 +57,38 @@ app.use(errorLogger);
 
 app.use(errors());
 
-app.use('/', (req, res) => {
-  res.status(404).send({ message: 'Recurso solicitado no encontrado' });
+app.use("/", (req, res) => {
+  res.status(404).send({ message: "Recurso solicitado no encontrado" });
 });
 app.use((err, req, res, next) => {
-  if (err.name === 'CastError') {
-    return res.status(400).send({ message: 'Se pasaron datos inválidos a los métodos para crear un usuario/tarjeta o actualizar el avatar/perfil de un usuario.' });
+  if (err.name === "CastError") {
+    return res
+      .status(400)
+      .send({
+        message:
+          "Se pasaron datos inválidos a los métodos para crear un usuario/tarjeta o actualizar el avatar/perfil de un usuario.",
+      });
   }
-  if (err.name === 'DocumentNotFoundError') {
-    return res.status(404).send({ message: 'No existe un usuario con el id solicitado o la solicitud se envió a una dirección inexistente;' });
+  if (err.name === "DocumentNotFoundError") {
+    return res
+      .status(404)
+      .send({
+        message:
+          "No existe un usuario con el id solicitado o la solicitud se envió a una dirección inexistente;",
+      });
   }
-  if(err.code === 11000){
-    return res.status(409).send({ message: 'Al registrarse, se especificó un correo electrónico que ya existe en el servidor' });
+  if (err.code === 11000) {
+    return res
+      .status(409)
+      .send({
+        message:
+          "Al registrarse, se especificó un correo electrónico que ya existe en el servidor",
+      });
   }
-  console.log(err)
-   res.status(500).send({message: 'Se ha producido un error en el servidor'});
- });
+  console.log(err);
+  res.status(500).send({ message: "Se ha producido un error en el servidor" });
+});
 
 app.listen(PORT, () => {
   console.log(`Escuchando por el puerto 3000`);
 });
-
